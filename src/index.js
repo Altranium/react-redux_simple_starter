3ae7076a67e76e05ed4c7cef7f0ea1943ed096b3
Alan Tran <dev@protoalpha.co>
2020-05-01 00:22:06 -0500

    Add list of videos to page

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
-
+import YTSearch from 'youtube-api-search';
+import VideoList from  './components/video_list';
 
 const YOUTUBE_API_KEY = 'AIzaSyC1hJpnoTOsj0z4CBugiHzCQ4GL2QhSobg';
 
 // Create a new component. This component should produce some HTML
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { videos:[] };
+
+        YTSearch({key: YOUTUBE_API_KEY, term:'surfboards'}, (videos) => {
+            this.setState( { videos });     // If key-value pair are the same name, ES6 can condense it to one word rather than it videos: videos 
+        });
+
+    }
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoList videos={this.state.videos}/>
+            </div>
+        );
+    }
+    
 
 }
 
